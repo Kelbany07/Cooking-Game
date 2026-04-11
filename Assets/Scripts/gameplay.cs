26b6a46638a9e4bf06fc6e2ffd1213eb7e113334
Kelbany07 <rachel.the.unipuppy@gmail.com>
2026-04-11 19:12:15 -0400

    Creating gamemanager

---
+++ Assets/Scripts/gameplay.cs
@@ -0,0 +1,27 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class gameplay : MonoBehaviour
+{
+
+    public static string cuttingboardS1;
+    public static string cuttingboardS2;
+    public static string cuttingboardS3;
+
+    public static string grillS1;
+    public static string grillS2;
+    public static string grillS3;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
